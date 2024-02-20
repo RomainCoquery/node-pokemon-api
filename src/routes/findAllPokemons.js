aba105d614e8d856f186e965ec14318a7580ffd7
Romain Coquery <romaincoquery@gmail.com>
2024-02-20 16:23:40 +0100

    Formulations de requêtes avancées

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,21 +1,56 @@
 const { Pokemon } = require('../db/sequelize');
+const { Op } = require('sequelize');
 
 module.exports = (app) => {
     app.get('/api/pokemons', (req, res) => {
-        Pokemon.findAll()
+        if(req.query.name) {
+            const name = req.query.name;
+            const limit = parseInt(req.query.limit) || 5;
+            const offset = parseInt(req.query.offset) || 0;
+            if(name.length < 2) {
+                const message = "Le terme de recherche doit contenir au moins 2 caractères.";
+                return res.status(400).json({ message: message });
+            }
+            Pokemon.findCountAll({ 
+                where: { 
+                    name: { // ce 'name' est la propriété du modèle pokémon
+                        [Op.like]: `%${name}%` // Ce 'name' est le critère de la recherche
+                    } 
+                },
+                order: [['name', 'ASC']],
+                limit: limit,
+                offset: offset 
+            })
+
+            .then(({count, rows}) => {
+                const pokemons = rows.map(pokemon => {
+                    pokemon = pokemon.get({ plain: true});
+                    delete pokemon.types_string;
+                    return pokemon;
+                });
+                const message = `Il y a ${count} pokémon(s) qui correspondent au terme de recherche '${name}'.`;
+                res.json({message: message, data: pokemons});
+            })
+            .catch(error => {
+                const message = "La liste des pokémons n'a pas pu être récupérée. Réesssayez dans quelques instants.";
+                res.status(500).json({message: message, data: error});
+            });
+        }
+        else {
+            Pokemon.findAll({ order: [['name', 'ASC']] })
             .then(result => {
                 const pokemons = result.map(pokemon => {
                     pokemon = pokemon.get({ plain: true});
                     delete pokemon.types_string;
                     return pokemon;
                 });
-                const message = 'La liste des pokémonsa a bien été récupérée.';
+                const message = "La liste des pokémons a a bien été récupérée.";
                 res.json({message: message, data: pokemons});
             })
-
             .catch(error => {
-                const message = `La liste des pokémons n'a pas pu être récupérée. Réesssayez dans quelques instants.`;
+                const message = "La liste des pokémons n'a pas pu être récupérée. Réesssayez dans quelques instants.";
                 res.status(500).json({message: message, data: error});
             });
+        }
     });
 }
